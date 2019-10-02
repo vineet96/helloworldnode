eb712e293e3c5503134154e2faccd798d4e88315
Vineet Agarwal <vineet_agarwal@mckinsey.com>
2019-10-02 11:34:14 -0700

    updated cloudbuild8

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello world vineet ji\n');
+  res.send('lag gai batti\n');
 });
 
 app.listen(PORT, HOST);
